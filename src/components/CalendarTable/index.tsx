baf18e766fcd1b952dac6b30b73326316363ec04
baranero <jakub.baran2@gmail.com>
2023-11-15 18:30:55 +0100

    added inline styles to css file

--- src/components/CalendarTable/index.tsx
+++ src/components/CalendarTable/index.tsx
@@ -7,8 +7,8 @@ import TotalRow from "../TotalRow";
 
 const CalendarTable: React.FC<{ nextDaysEvents: DayEvents[] }> = ({ nextDaysEvents }) => {
   return (
-    <TableContainer data-testid="calendar-summary" style={{ boxShadow: "0 0 10px 10px #e0e0e0" }}>
-      <Table sx={{ minWidth: 600 }}>
+    <TableContainer className="table-container" data-testid="calendar-summary">
+      <Table className="table">
         <TableHeader />
         <TableBody>
           {nextDaysEvents.map((dayEvents, index) => (
